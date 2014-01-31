f18ca490120b66c8f9190abfd66a08c7de886342
bill-turner <billturner.github@gmail.com>
2014-01-30 22:31:55 -0600

    overhaul numero uno

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Resume::Application.routes.draw do
+  get 'welcome/index' => 'welcome#index'
+  get 'welcome/data_analysis'  => 'welcome#data_analysis'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
